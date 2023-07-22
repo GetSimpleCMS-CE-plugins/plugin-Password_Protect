0190e7039953cb8319493d7bc34b25a7b6b7479f
risingisland <risingisland@yahoo.com>
2023-07-22 10:48:29 +0100

    Add files via upload

---
+++ password_protect/lang/es_ES.php
@@ -0,0 +1,9 @@
+<?php
+
+$i18n = [
+    "PASSWORD_TITLE"        => "Esta página está protegida con contraseña",
+    "ENTER_PASSWORD"        => "Ingrese contraseña:",
+    "PASSWORD_PLACEHOLDER"  => "Contraseña",
+    "SUBMIT_BUTTON"         => "Desbloquear",
+    "WRONG_PASSWORD"        => "<p style='color: red; font-weight: bold;'>¡Contraseña incorrecta!</p>"
+];
